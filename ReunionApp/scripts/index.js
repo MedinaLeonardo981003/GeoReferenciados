35cf96d9cad824f3df6829316aff3f315cfb5425
LeonardoMedina <lmedina5859@gmail.com>
2020-06-10 20:30:15 -0500

    Commit finalP

--- ReunionApp/scripts/index.js
+++ ReunionApp/scripts/index.js
@@ -18,87 +18,7 @@
      });
 
 
-     var propiedades = {
-       center: {
-         lat: 21.152639,
-         lng: -101.711598
-       },
-       zoom: 12
-
-     };
-
-     var mapa = document.getElementById("map");
-
-     var map = new google.maps.Map(mapa, propiedades);
-
-     var icono = {
-       url: "https://media.giphy.com/media/1ZweHMlSaxcc32mSSu/giphy.gif",
-       scaledSize: new google.maps.Size(25, 25),
-       origin: new google.maps.Point(0, 0),
-       anchor: new google.maps.Point(0, 0)
-     };
-
-     var marker = new google.maps.Marker({
-       position: {
-         lat: 0,
-         lng: 0
-       },
-       icon: icono,
-       map: map
-     });
-
-     var watchId = null;
-
-     const boton = document.getElementById('boton');
-
-     var positionOptions = {
-       enableHighAccuracy: true,
-       timeout: 10 * 1000, //10 segundos
-       maximumAge: 30 * 1000 //30 segundos
-     };
-
-
-     if (navigator.geolocation) {
-
-
-       watchId = navigator.geolocation.watchPosition(function (position) {
-         var lat = position.coords.latitude;
-         var lng = position.coords.longitude;
-
-         console.log(position);
-
-         var coordenadas = lat + ',' + lng;
-
-         var exactitud = position.coords.accuracy ? position.coords.accuracy : 'no disponible';
-         var altitud = position.coords.altitude ? position.coords.altitude : 'no disponible';
-         var velocidad = position.coords.speed ? position.coords.speed : 'no disponible';
-         var fechayhora = (new Date(position.timestamp)).toString();
-
-
-         const html = `
-                  <p>Coordenadas: ${ coordenadas }</p>
-                  <p>Exactitud: ${ exactitud }</p>
-                  <p>Altitud: ${ altitud }</p>
-                  <p>Velocidad: ${ velocidad }</p>
-                  <p>Fecha y hora: ${ fechayhora }</p>
-              `;
-
-         const datos = document.getElementById('datos');
-         account.innerHTML = html;
-
-         marker.setPosition(new google.maps.LatLng(lat, lng));
-         map.panTo(new google.maps.LatLng(lat, lng));
-
-       }, error, positionOptions);
-     }
-
-
-     function error(positioError) {
-       console.log(positioError.messsage);
-     }
-
-
-
+     iniciaMapa();
 
      listaloggedin.forEach(item => item.style.display = 'block');
      listaloggedout.forEach(item => item.style.display = 'none');
@@ -108,5 +28,85 @@
      datosdelacuenta.innerHTML = '';
      listaloggedin.forEach(item => item.style.display = 'none');
      listaloggedout.forEach(item => item.style.display = 'block');
+   }
+ }
+
+ function iniciaMapa() {
+
+   var propiedades = {
+     center: {
+       lat: 21.152639,
+       lng: -101.711598
+     },
+     zoom: 12
+
+   };
+
+   var mapa = document.getElementById("map");
+
+   var map = new google.maps.Map(mapa, propiedades);
+
+   var icono = {
+     url: "https://media.giphy.com/media/1ZweHMlSaxcc32mSSu/giphy.gif",
+     scaledSize: new google.maps.Size(25, 25),
+     origin: new google.maps.Point(0, 0),
+     anchor: new google.maps.Point(0, 0)
+   };
+
+   var marker = new google.maps.Marker({
+     position: {
+       lat: 0,
+       lng: 0
+     },
+     icon: icono,
+     map: map
+   });
+
+   var watchId = null;
+
+
+   var positionOptions = {
+     enableHighAccuracy: true,
+     timeout: 10 * 1000, //10 segundos
+     maximumAge: 30 * 1000 //30 segundos
+   };
+
+
+   if (navigator.geolocation) {
+
+     watchId = navigator.geolocation.watchPosition(function (position) {
+       var lat = position.coords.latitude;
+       var lng = position.coords.longitude;
+
+       console.log(position);
+
+       var coordenadas = lat + ',' + lng;
+
+       var exactitud = position.coords.accuracy ? position.coords.accuracy : 'no disponible';
+       var altitud = position.coords.altitude ? position.coords.altitude : 'no disponible';
+       var velocidad = position.coords.speed ? position.coords.speed : 'no disponible';
+       var fechayhora = (new Date(position.timestamp)).toString();
+
+
+       const html = `
+                  <p>Coordenadas: ${ coordenadas }</p>
+                  <p>Exactitud: ${ exactitud }</p>
+                  <p>Altitud: ${ altitud }</p>
+                  <p>Velocidad: ${ velocidad }</p>
+                  <p>Fecha y hora: ${ fechayhora }</p>
+              `;
+
+       const datos = document.getElementById('datos');
+       datos.innerHTML = html;
+
+       marker.setPosition(new google.maps.LatLng(lat, lng));
+       map.panTo(new google.maps.LatLng(lat, lng));
+
+     }, error, positionOptions);
+   }
+
+
+   function error(positioError) {
+     console.log(positioError.messsage);
    }
  }
