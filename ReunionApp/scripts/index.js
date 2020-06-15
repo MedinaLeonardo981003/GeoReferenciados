6d0d4f2c2623a98631bff7679d9b800e26d3cd8f
LeonardoMedina <lmedina5859@gmail.com>
2020-06-15 17:49:33 -0500

    Commit finalP2

--- ReunionApp/scripts/index.js
+++ ReunionApp/scripts/index.js
@@ -27,8 +27,8 @@
      arreglo = [];
 
      var emialuser = user.email;
-     var count = 0;
-     console.log("Cambio1");
+     var count;
+     console.log("Cambio2");
      db.collection('reuniones').get().then(doc => {
        doc.docs.forEach(doc => {
          console.log("Mi id:  " + doc.id);
